6415d9e00b842543f7a92b1ecd62dcc63c03a458
Bartosz Gordon <bartosz.gordon@sealcode.org>
2017-11-29 19:11:59 +0100

    Dodano kolekcję products
    Summary: Ref T528
    
    Test Plan: Sprawdzić, czy kolekcja jest zaimplementowana zgodnie z opisem
    
    Reviewers: kuba-orlik
    
    Reviewed By: kuba-orlik
    
    Maniphest Tasks: T528
    
    Differential Revision: https://hub.sealcode.org/D132

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,7 @@ const Sealious = require("sealious");
 
 const App = new Sealious.App();
 
-const dependencies = ["collections/users.js"];
+const dependencies = ["collections/users.js", "collections/products.js"];
 
 dependencies.forEach(dependency => require(`./${dependency}`)(App));
 
---
+++ src/backend/collections/products.js
@@ -0,0 +1,21 @@
+module.exports = function(App) {
+	const Products = App.createCollection({
+		name: "products",
+		fields: [
+			{ name: "name", type: "text", required: true },
+			{
+				name: "calories_per_100g",
+				type: "int",
+				params: {
+					min: 1,
+				},
+				required: true,
+			},
+		],
+		access_strategy: {
+			default: "logged_in",
+			update: "noone",
+			delete: "noone",
+		},
+	});
+};
